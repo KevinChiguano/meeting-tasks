4382c65d3f6c6e220ae075164e1d0b747b6a5c01
KevinChiguano <kevin_chili1998@hotmail.com>
2026-06-04 12:28:37 -0500

    chore: configurar puerto y soporte REDIS_DB

--- src/core/redis.ts
+++ src/core/redis.ts
@@ -4,6 +4,7 @@ const redisConnection = new Redis({
   host: process.env.REDIS_HOST || '127.0.0.1',
   port: parseInt(process.env.REDIS_PORT || '6379', 10),
   password: process.env.REDIS_PASSWORD || undefined,
+  db: parseInt(process.env.REDIS_DB || '0', 10),
   maxRetriesPerRequest: null, // BullMQ requiere esto en null
 });
 
